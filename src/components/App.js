7de8bce171531d7b404ea19758f9f6e4e86c8d21
+ <rgigs123@gmail.com>
2025-08-22 20:28:45 +0300

    App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState("");
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setDogImage(data.message); // the API returns 
+      });
+  }, []); 
+  //[] → making  sure thie fetch only runs once when the component mounts
+
+  return (
+    <div>
+      
+      {dogImage ? (
+        <img src={dogImage} alt="A Random Dog" width="300" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
